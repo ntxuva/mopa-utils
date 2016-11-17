345a618fec5077501243c3d279035f15fb1ff3eb
Paulo Phagula <paulo@ux.co.mz>
2016-11-17 22:55:14 +0200

    hush curl

--- jobs/check_if_answers_were_received.sh
+++ jobs/check_if_answers_were_received.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-curl -X GET "http://127.0.0.1:5000/tasks/check-if-answers-were-received" --header "API_KEY: $API_KEY"
+curl -s -S -X GET "http://127.0.0.1:5000/tasks/check-if-answers-were-received" --header "API_KEY: $API_KEY"
--- jobs/send_daily_survey.sh
+++ jobs/send_daily_survey.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-curl -X GET "http://127.0.0.1:5000/tasks/send-daily-survey" --header "API_KEY: $API_KEY"
+curl -s -S -X GET "http://127.0.0.1:5000/tasks/send-daily-survey" --header "API_KEY: $API_KEY"
