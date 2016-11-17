16dffbb3e0104408ffcce73a10d73092fc00f9b7
Paulo Phagula <pphagula@gmail.com>
2016-11-17 12:51:13 +0200

    don't POST on wrong URL. GET on the right one

--- jobs/check_if_answers_were_received.sh
+++ jobs/check_if_answers_were_received.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-curl -X POST "http://127.0.0.1:5000/api/task/check-if-answers-were-received" --header "API_KEY: $API_KEY"
+curl -X GET "http://127.0.0.1:5000/tasks/check-if-answers-were-received" --header "API_KEY: $API_KEY"
--- jobs/send_daily_survey_replies.sh
+++ jobs/send_daily_survey_replies.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-curl -X POST "http://127.0.0.1:5000/api/task/send-daily-survey-replies" --header "API_KEY: $API_KEY"
+curl -X GET "http://127.0.0.1:5000/tasks/send-daily-survey-replies" --header "API_KEY: $API_KEY"
