25d64a5327dc9b4192b16d21fd8a75dcb472df92
Paulo Phagula <pphagula@gmail.com>
2016-11-18 12:55:21 +0200

    improve logging configuration

--- mopa/__init__.py
+++ mopa/__init__.py
@@ -7,20 +7,62 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_reggie import Reggie
 from flask_cors import CORS
 
-from werkzeug.debug import DebuggedApplication
-from werkzeug.contrib.cache import SimpleCache
-
-import yaml
-import logging
-from logging.config import dictConfig
+import logging, logging.config, yaml
+from logging.handlers import RotatingFileHandler, SMTPHandler
 
 # dependencies / extensions
 db = SQLAlchemy()
 reggie = Reggie()
 cors = CORS()
-cache = SimpleCache()
 
 from .infrastructure import (CustomJSONEncoder)
+
+
+def setup_logging(app):
+    config = app.config
+
+    date_format = '%Y-%m-%d %H:%M:%S'
+    simple_format = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    complex_format = """
+        Message type:       %(levelname)s
+        Location:           %(pathname)s:%(lineno)d
+        Module:             %(module)s
+        Function:           %(funcName)s
+        Time:               %(asctime)s
+
+        Message:
+
+        %(message)s
+    """
+    simple_formatter = logging.Formatter(simple_format, date_format)
+    complex_formatter = logging.Formatter(complex_format, date_format)
+
+    log_file_path = os.path.join(app.config['APP_ROOT'],'data/logs/mopa.log')
+
+    file_handler = RotatingFileHandler(log_file_path, maxBytes=10485760, backupCount=10)
+    file_handler.setLevel(logging.DEBUG)
+    file_handler.setFormatter(simple_formatter)
+
+    mail_handler = SMTPHandler(
+        (config['SMTP_USERNAME'], config['SMTP_PASSWORD']),
+        config['SMTP_USERNAME'],
+        config['ADMINS'],
+        '[Mopa] Bug log',
+        credentials=(config['SMTP_USERNAME'], config['SMTP_PASSWORD']),
+        secure=()
+    )
+    mail_handler.setLevel(logging.ERROR)
+    mail_handler.setFormatter(complex_formatter)
+
+    # logging.config.dictConfig(yaml.load(file(os.path.join(app.config['BASE_DIR'], '../logging.yaml'),'r')))
+    flask_cors_logger = logging.getLogger('flask_cors')
+    flask_cors_logger.level = logging.CRITICAL
+    loggers = [app.logger, flask_cors_logger, logging.getLogger('root')]
+    for logger in loggers:
+        logger.addHandler(file_handler)
+        if not app.debug:
+            logger.addHandler(mail_handler)
+
 
 def create_app(config_name=None, main=True):
     """Create an application instance."""
@@ -30,17 +72,11 @@ def create_app(config_name=None, main=True):
     app.json_encoder = CustomJSONEncoder
     app.config['JSON_PRETTYPRINT_REGULAR'] = False
     app.config.from_pyfile(cfg)
-
-    try:
-        logging.config.dictConfig(yaml.load(file(os.path.join(app.config['BASE_DIR'], '../logging.yaml'),'r')))
-    except Exception, e:
-        pass
-        # print "Error configuring logger ", str(e)
+    setup_logging(app)
 
     # initialize extensions
     db.init_app(app)
     reggie.init_app(app)
-    # logging.getLogger('flask_cors').level = logging.CRITICAL
     # cors.init_app(app, resources={r"/api/*": {"origins": "*"}})
 
     # One line of code to cut our Flask page load times by 60%
