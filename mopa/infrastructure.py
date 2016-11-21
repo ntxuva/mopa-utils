08ad83ece9e3f151a2bc988e90ba04c6f0904f22
Paulo Phagula <pphagula@gmail.com>
2016-11-21 10:01:26 +0200

    Add plain text contents to email before html
    1. Why is this change necessary?
    To stop gmail from showing both html and plaintext emails contents
    
    2. How does it address the issue?
    By having text plain content first and html last gmail somehow
    display emails correctly, or at least that's what most threads say
    
    3. What side effects does this change have?
    None
    
    Relates to #14

--- mopa/infrastructure.py
+++ mopa/infrastructure.py
@@ -309,12 +309,12 @@ def send_mail(to, subject, message, is_html=False, cc=None, bcc=None, reply_to=N
     if not is_html:
         mail.attach(MIMEText(message, 'plain', 'utf-8'))
     else:
-        mail.attach(MIMEText(message, 'html', 'utf-8'))
-
         if text:
             mail.attach(MIMEText(text, 'plain', 'utf-8'))
         else:
             mail.attach(MIMEText(html2text_converter.handle(message), 'plain', 'utf-8'))
+
+        mail.attach(MIMEText(message, 'html', 'utf-8'))
 
     if attachments:
         attachments = attachments if isinstance(attachments, list) else map(str.strip, attachments.split(','))
