0953023fd7972d306d7294e2816e20f20966a406
Paulo Phagula <pphagula@gmail.com>
2017-05-30 17:18:54 +0200

    Ensure DB Schema is created before running tests

--- mopa/data/db/mopa_reports_schema.sql
+++ mopa/data/db/mopa_reports_schema.sql
@@ -1,3 +1,4 @@
+/*
 -- Drupal
 SELECT * FROM node WHERE `type`='report';
 
@@ -8,24 +9,25 @@ content_type_* and content_field_*
 SELECT * FROM  node_type;
 SELECT * FROM field_data_body WHERE bund;
 -- Mopa Specific
+*/
 
 DROP TABLE IF EXISTS mopa_reports;
 CREATE TABLE mopa_reports(
   -- _id INT AUTO_INCREMENT ,
 	id VARCHAR(255) PRIMARY KEY,
 	district TEXT,
-	neighbourhood TEXT, 
-	location_name TEXT, 
+    neighbourhood TEXT,
+    location_name TEXT,
 	nature TEXT,
-	requested_datetime DATETIME, 
-	updated_datetime DATETIME, 
-	type TEXT, 
+    requested_datetime DATETIME,
+    updated_datetime DATETIME,
+    type TEXT,
 	status TEXT,
 	status_notes TEXT
 );
 
+/*
 SELECT * FROM mopa_reports;
-
 DELETE FROM mopa_reports;
 
 
@@ -33,21 +35,21 @@ DELETE FROM mopa_reports;
 SELECT recent.*, recent.tempo_medio_resolucao-old.tempo_medio_resolucao as variacao
 FROM
 (
-SELECT type, 
-	COUNT(*) as no_occorencias, 
+SELECT type,
+    COUNT(*) as no_occorencias,
 	(COUNT(*)/b.total_reports * 100) as `pct_do_total`,
   AVG(a.time_diff) `tempo_medio_resolucao`
-FROM mopa_reports 
+FROM mopa_reports
   JOIN (SELECT id, TIMESTAMPDIFF(HOUR, requested_datetime, updated_datetime) time_diff FROM mopa_reports) AS a
     ON mopa_reports.id=a.id
   JOIN (SELECT COUNT(*) as total_reports FROM mopa_reports WHERE requested_datetime BETWEEN '2015-09-20' AND '2015-09-26') b
 WHERE requested_datetime BETWEEN '2015-09-20' AND '2015-09-26'
 GROUP BY type
-) AS recent JOIN 
+) AS recent JOIN
 (
-SELECT type,  
+SELECT type,
   AVG(a.time_diff) `tempo_medio_resolucao`
-FROM mopa_reports 
+FROM mopa_reports
   JOIN (SELECT id, TIMESTAMPDIFF(HOUR, requested_datetime, updated_datetime) time_diff FROM mopa_reports) AS a
     ON mopa_reports.id=a.id
   JOIN (SELECT COUNT(*) as total_reports FROM mopa_reports WHERE requested_datetime BETWEEN '2015-09-13' AND '2015-09-19') b
@@ -60,10 +62,10 @@ ON recent.type=old.type;
 SELECT recent.*, recent.tempo_medio_resolucao - old.tempo_medio_resolucao as variacao_do_tempo_medio
 FROM
 (
-SELECT 
-	district, 
+SELECT
+    district,
 	neighbourhood,
-	COUNT(*) as no_occorencias, 
+    COUNT(*) as no_occorencias,
 	ROUND((COUNT(*)/b.total_reports * 100),2) as `pct_do_total`,
 	AVG(a.time_diff) `tempo_medio_resolucao`
 FROM mopa_reports
@@ -75,9 +77,9 @@ WHERE TRIM(LOWER(district)) IN ('kamaxaquene', 'kamubukwana', 'katembe', 'inhago
 GROUP BY district, neighbourhood
 ) AS recent JOIN
 (
-SELECT 
-	district, 
-	neighbourhood, 
+SELECT
+    district,
+    neighbourhood,
 	COUNT(*) as no_occorencias,
 	ROUND((COUNT(*)/b.total_reports * 100),2) as `pct_do_total`,
 	AVG(a.time_diff) `tempo_medio_resolucao`
@@ -88,12 +90,12 @@ FROM mopa_reports
 WHERE TRIM(LOWER(district)) IN ('kamaxaquene', 'kamubukwana', 'katembe', 'inhagoia b')
   AND requested_datetime BETWEEN '2015-09-13' AND '2015-09-19'
 GROUP BY district, neighbourhood
-) as old 
+) as old
 ON recent.district=old.district AND recent.neighbourhood=old.neighbourhood;
 
 -- Tiago Report
-SELECT 
-	neighbourhood as bairro, 
+SELECT
+    neighbourhood as bairro,
   nature as problema,
   SUM(CASE WHEN status='Registado' THEN 1 ELSE 0 END) AS registado,
   SUM(CASE WHEN status='Em processo' THEN 1 ELSE 0 END) AS em_processo,
@@ -140,8 +142,8 @@ SELECT * FROM mopa_reports WHERE requested_datetime BETWEEN '2015-10-17' AND '20
 
 SELECT * FROM mopa_reports LIMIT 1;
 
-SELECT a.*, @curRank := @curRank + 1 AS rank 
-FROM 
+SELECT a.*, @curRank := @curRank + 1 AS rank
+FROM
 (
 SELECT location_name, COUNT(*) as count
 FROM mopa_reports
@@ -178,3 +180,4 @@ WHERE requested_datetime BETWEEN '2015-10-20' AND '2015-10-27'
 GROUP BY type
 ) AS old
 ON recent.type=old.type
+*/
